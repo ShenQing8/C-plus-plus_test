fcb0073bf5b89e652f3247512fe2d0d97f5e0ddf
SQ <1653216274@qq.com>
2024-09-27 18:50:09 +0800

    test

--- HelloWOrld/HelloWorld/HelloWorld.cpp
+++ HelloWOrld/HelloWorld/HelloWorld.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 #define NUM 10
 
 
 int main()
 {
-	char a[5] = "yeah";
-	std::cout << "Hello World!" << a << endl;
-	printf("Hello World!\n");
+	char arr1[5] = "yeah";
+	std::cout << "Hello World!" << arr1 << endl;
+	string str = "yeah";
+	//printf("Hello World! %s\n", str);
+	cout << str << endl;
 
 	int* b = (int*)malloc(NUM * sizeof(int));
 	if (b == NULL)
@@ -37,5 +40,30 @@ int main()
 		cout << '\n';
 	}
 
+	int x = 1;
+	int y = 2;
+	const int* p1 = &x;
+	p1 = &y;
+	//*p1 = 3;
+	int* const p2 = &x;
+	//p2 = &y;
+	*p2 = 3;
+
+	//(char[5])* p3 = a;
+	//char* p[5] = a;
+	//char* [5] p = a;
+	//char[5] * p = a;
+
+	int* p = new int(5);
+	cout << *p << endl;
+	int* arr_p = new int[5] {1, 2, 3, 4, 5};
+	for (size_t i = 0; i < 5; i++)
+	{
+		cout << *(arr_p + i) << "\n";
+	}
+	delete p;
+	delete[]arr_p;
+
+	system("pause");
 	return 0;
 }
