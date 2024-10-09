c6a8be2f0cc61e8e740b7ee998b530b2bd29c594
SQ <1653216274@qq.com>
2024-10-09 17:36:47 +0800

    test

--- Object_Oriented_encapsulation/Project1/封装.cpp
+++ Object_Oriented_encapsulation/Project1/封装.cpp
@@ -15,50 +15,99 @@ using namespace std;
 //struct 的成员默认权限为公共 public
 //class  的成员默认权限为私有 private
 
+//默认情况下，创建类时，编译器会提供至少3个函数：默认构造、拷贝构造、析构
+//如用户自定义构造函数，则编译器不提供默认构造
+
+
 class Student
 {
 public:
-	void setName(string name)
+	Student()
 	{
-		Name = name;
+		cout << "默认构造调用" << endl;
 	}
-	string getName()
+	//初始化列表
+	Student(string name, int age, string id, string sex) :m_Name(name), m_Age(age), m_Id(id), m_Sex(sex)
 	{
-		return Name;
+		cout << "参数构造调用" << endl;
+		//m_Age = age;
+	}
+	Student(const Student& s)
+	{
+		cout << "拷贝构造调用" << endl;
+		m_Name = s.m_Name;
+		m_Age = s.m_Age;
+		m_Id = s.m_Id;
+		m_Sex = s.m_Sex;
+	}
+	~Student()
+	{
+		cout << "析构函数调用" << endl;
+	}
+
+	//静态成员函数不可直接访问 非 静态成员变量
+	static void func(Student& s)//静态成员函数也有访问权限，同静态成员变量
+	{
+		School = "UPUPUP";
+		s.m_Age = 19;
+	}
+	//this指针
+	//this指针指向 被调用的成员函数 所属的 对象
+	Student& Addage(Student& s)
+	{
+		this->m_Age += s.m_Age;
+		return *this;
 	}
 	void setId(string id)
 	{
-		Id = id;
+		m_Id = id;
 	}
 	string getSex()
 	{
-		return Sex;
+		return m_Sex;
 	}
-	void setAge(int age)
-	{
-		Age = age;
-	}
-	int getAge()
-	{
-		return Age;
-	}
+	string m_Name;//可读可写
+	int m_Age;    //可读可写
+	static string School; //静态的成员变量，所有对象共享同一份数据，类内声明，类外初始化
+
 	//将成员设置为私有，可便于管理
 private:
-	string Name;//可读可写
-	string Id;  //只写
-	string Sex; //只读
-	int Age;    //可读可写
+	string m_Id;  //只写
+	string m_Sex; //只读
+	static int School_Num; //当静态成员变量设置为私有时，外界访问不到
 };
+string Student::School = "UUU";
+int Student::School_Num = 123456;
+
+int& func(int x)
+{
+	x += 10;
+	return x;
+}
 
 int main()
 {
-	Student stu;
-	//stu.Name = "张三";
-	//stu.Id = "202366666";
-	//stu.Age = 20;
-	//stu.Sex = "男";
+	int a = 0;
+	a = func(a);
+	int& b = func(a);
+	cout << a << endl;
+	cout << b << endl;
 
+
+	Student stu("张三", 18, "2023666666", "男");
+
+	Student stu2(stu);
+	cout << stu2.m_Age<< endl;
 	
+	//静态成员访问
+	cout << stu.School << endl;
+	Student::func(stu);
+	cout << Student::School << endl;
+	cout << stu.m_Age<< endl;
+
+	//this指针应用
+	stu2.Addage(stu).Addage(stu).Addage(stu);
+	cout << stu2.m_Age << endl;
 
 	return 0;
 }
---
+++ Object_Oriented_encapsulation/深拷贝与浅拷贝/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+//浅拷贝：多个指针指向同一块内存
+//深拷贝：多个指针指向不同的内存
